f08b439da317fcdb09d7d78eab71a969a566b47b
arahim <arahim@MotionE-Service.duttahh.local>
2019-08-09 14:23:39 +0200

    first commit

--- src/main/java/com/user/Exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/user/Exception/CustomizedResponseEntityExceptionHandler.java
@@ -20,6 +20,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
+	//response for UserNotFoundException
 	@ExceptionHandler(UserNotFoundException.class)
 	public final ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
 		ExceptionResponse exceptionResponse = new ExceptionResponse((new Date()), ex.getMessage(), request.getDescription(false));
